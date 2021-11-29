58e4eb8805acd1c21ed06840446ad0952dc95e4c
cplagge2 <93496077+cplagge2@users.noreply.github.com>
2021-11-28 23:05:04 -0600

    Add files via upload
    Replaced confidential information with placeholders.

---
+++ CONFIG.java
@@ -0,0 +1,8 @@
+/**
+ * Class containing login information for use in testing
+ */
+public class CONFIG {
+  protected static String subdomain = "Placeholder"; //TODO update subdomain
+  protected static String username = "Placeholder"; //TODO update username
+  protected static String token = "Placeholder"; //TODO update token
+}
